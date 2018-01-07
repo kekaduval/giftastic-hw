d84736119da6d6a09d3afb8632f17560f46602e6
kekaduval <32457817+kekaduval@users.noreply.github.com>
2018-01-07 17:38:41 -0500

    last changes

---
+++ assets/javascript/gif.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+var movies = ["phineas and pherb", "Desperate housewives", "Game of thrones", "Charmed", "Modern family", "Friends", "Gossip girl", "Gilmore girls", "American dad"];
+
+$(document).on("click", function(){
+  console.log(this)
+})
+
+
+function displayMovie() {
+    $(".gifs-appear-here").empty();
+    var movie = $(this).attr("data-movie");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movies + "&api_key=dc6zaTOxFJmzC&limit=10";
+$('.gifs-appear-here').append('<h2 class="result_title">Now Playing: <span>' + $(this).data('movie') + ' Gifs!</span></h2>' );
+  console.log(movie);
+  console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response.data);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+
+                var movieGif = $("<img>");
+                movieGif.addClass("gifClick");
+                movieGif.attr({
+                    src: results[i].images.fixed_height.url,
+                    "data-still": results[i].images.fixed_height_still.url,
+                    "data-animate": results[i].images.fixed_height.url,
+                    "data-state": "still",
+                });
+
+            gifDiv.append(p);
+            gifDiv.append(movieGif);
+            $(".gifs-appear-here").append(gifDiv);
+        }
+        var row = $('div.gifs-appear-here > div');
+
+        for (var j = 0; j < row.length; j += 2) {
+            row.slice(j, j + 2).wrapAll('<div class="row mx-auto justify-content-between"></div>');
+        }
+    })
+};
+
+$(document).on("click", ".gifClick", function() {
+
+    var state = $(this).attr("data-state");
+    console.log(this);
+    if (state == "still") {
+
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+    } else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+    }
+})
+
+function makeButtons() {
+    $(".buttons").empty();
+    for (var i = 0; i < movies.length; i++) {
+        var button = $("<button type='button' class='btn'>");
+        button.addClass("buttonClass");
+        button.attr("data-movie", movies[i]);
+        button.append(movies[i]);
+        $(".buttons").append(button);
+    }
+}
+
+$(document).on("click", ".buttonClass", displayMovie);
+
+$("#add-movie").on("click", function(event) {
+    event.preventDefault();
+    var newMovie = $("#movie-input").val().trim();
+    if (newMovie !== "") {
+        $("#movie-input").val("");
+        movies.push(newMovie);
+    }
+    makeButtons();
+})
+makeButtons();
+
+});
